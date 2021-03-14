c143ffb9dd3175d9c8975c67c3c2e4ea8f8e44c6
sean <pulpfictionost@gmail.com>
2021-03-14 20:52:47 +0000

    Use 'if let' pattern instead of match in the TxProcessor

--- src/tx_processor.rs
+++ src/tx_processor.rs
@@ -3,7 +3,10 @@ use crate::tx_stream_reader::TxStreamReader;
 use crate::Transaction;
 use crate::DECIMAL_PRECISION;
 
-pub struct TxProcessor<T> where T: std::io::Read {
+pub struct TxProcessor<T>
+where
+    T: std::io::Read,
+{
     tx_stream: TxStreamReader<T>,
     acc_man: AccountManager,
 }
@@ -15,23 +18,15 @@ impl<T: std::io::Read> TxProcessor<T> {
 
     pub async fn start(&mut self) {
         for buf in self.tx_stream.stream.records() {
-            match buf {
-                Ok(tx) => {
-                    match tx.deserialize::<Transaction>(None) {
-                        Ok(mut deserialized_tx) => {
-                            deserialized_tx.amount = match deserialized_tx.amount {
-                                Some(a) => Some(a.round_dp(DECIMAL_PRECISION)),
-                                None => None,
-                            };
-                            match self.acc_man.process_tx(&deserialized_tx) {
-                                Ok(_) => {}
-                                Err(e) => eprintln!("Error: {} : {:?}", e, tx),
-                            };
-                        }
-                        Err(e) => eprintln!("Error: {} : {:?}", e, tx),
-                    };
+            if let Ok(tx) = buf {
+                if let Ok(mut deserialized_tx) = tx.deserialize::<Transaction>(None) {
+                    if let Some(amt) = deserialized_tx.amount {
+                        deserialized_tx.amount = Some(amt.round_dp(DECIMAL_PRECISION));
+                    }
+                    if let Err(e) = self.acc_man.process_tx(&deserialized_tx) {
+                        eprintln!("Error processing transaction: {}", e);
+                    }
                 }
-                Err(e) => eprintln!("Could not read line: {}", e),
             }
         }
     }
